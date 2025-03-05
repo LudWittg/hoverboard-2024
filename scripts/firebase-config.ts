c46968f31189976c0353d8ed33d59878db503dc3
Ludwittge <16162947+LudWittg@users.noreply.github.com>
2025-03-05 01:15:12 +0100

    Update firebase-config.ts

--- scripts/firebase-config.ts
+++ scripts/firebase-config.ts
@@ -4,7 +4,20 @@ import { cert, initializeApp, ServiceAccount } from 'firebase-admin/app';
 // https://github.com/import-js/eslint-plugin-import/issues/1810
 // eslint-disable-next-line import/no-unresolved
 import { getFirestore } from 'firebase-admin/firestore';
-import serviceAccount from '../serviceAccount.json';
+//import serviceAccount from '../serviceAccount.json';
+
+let serviceAccount = {
+    "type": "service_account",
+    "project_id": "test-project",
+    "private_key_id": "fake-key-id",
+    "private_key": "fake-key",
+    "client_email": "fake@test-project.iam.gserviceaccount.com",
+    "client_id": "123456789",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/test-project.iam.gserviceaccount.com"
+};
 
 const credential = cert(serviceAccount as ServiceAccount);
 initializeApp({ credential });
